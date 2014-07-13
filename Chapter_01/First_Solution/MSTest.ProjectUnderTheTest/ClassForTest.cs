ab06660896e3483a3d236cbe230fce4dfc8f8131
Nick Lototskiy <nick.lototskiy@gmail.com>
2014-07-13 08:21:36 +0300

    Commit first chapter

---
+++ Chapter_01/First_Solution/MSTest.ProjectUnderTheTest/ClassForTest.cs
@@ -0,0 +1,10 @@
+﻿namespace MSTest.ProjectUnderTheTest
+{
+    public class ClassForTest
+    {
+        public int SumOfTwoNumbers(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+    }
+}
---
+++ Chapter_01/First_Solution/MSTest.UnitTest/MSTest.FirstTestSuite.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using MSTest.ProjectUnderTheTest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MSTest.UnitTest
+{
+    [TestClass]
+    public class FirstTestSuite
+    {
+        [TestMethod]
+        public void SumTwoPositiveNumbersTest()
+        {
+            // Arrange
+            Trace.WriteLine("Test start");
+            const int firstNumber = 1;
+            const int secondNumber = 3;
+            var testClass = new ClassForTest();
+
+            // Act
+            int result = testClass.SumOfTwoNumbers(firstNumber, secondNumber);
+            
+            //Assert
+            Assert.AreEqual(4, result);
+            Trace.WriteLine("Test finished");
+        }
+    }
+}
