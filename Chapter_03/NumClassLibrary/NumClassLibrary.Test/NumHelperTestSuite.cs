c64b93055d203fdee55f4cf386f97185088b2b68
Nick Lototskiy <nick.lototskiy@gmail.com>
2014-08-03 13:32:04 +0300

    Added chapter 3 code.

---
+++ Chapter_03/NumClassLibrary/NumClassLibrary.Test/NumHelperTestSuite.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NumClassLibrary.Test
+{
+    [TestClass]
+    public class NumHelperTestSuite
+    {
+        private NumHelper _helperUnderTheTest;
+
+        [TestInitialize]
+        public void TestSetup()
+        {
+            _helperUnderTheTest = new NumHelper();
+        }
+
+        [TestMethod]
+        public void GetFactorialOfPositiveNumber_CheckResult_PositiveTest()
+        {
+            const int inputNumber = 5;
+            const long expectedResult = 120;
+
+            long actualResult = _helperUnderTheTest.Factorial(inputNumber);
+
+            Assert.AreEqual(expectedResult, actualResult, string.Format("Invalid result of Factorial method execution. Expected {0}. Actual {1} ", expectedResult, actualResult));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetFactorialOfNegativeNumber_RiseInvalidArgumentException_NegativeTest()
+        {
+            const int inputNumber = -2;
+
+            _helperUnderTheTest.Factorial(inputNumber);
+
+            Assert.Fail("Invalid argument exception was not appear.");
+        }
+
+        [TestMethod]
+        public void GetFactorialOfZero_ReturnsOne_PositiveTest()
+        {
+            const int inputNumber = 0;
+            const long expectedResult = 1;
+
+            long actualResult = _helperUnderTheTest.Factorial(inputNumber);
+
+            Assert.AreEqual(expectedResult, actualResult, string.Format("Invalid result of Factorial method execution. Expected {0}. Actual {1} ", expectedResult, actualResult));
+        }
+
+        [TestMethod]
+        public void GetFactorialOfOne_ReturnsOne_PositiveTest()
+        {
+            const int inputNumber = 1;
+            const long expectedResult = 1;
+
+            long actualResult = _helperUnderTheTest.Factorial(inputNumber);
+
+            Assert.AreEqual(expectedResult, actualResult, string.Format("Invalid result of Factorial method execution. Expected {0}. Actual {1} ", expectedResult, actualResult));
+        }
+    }
+}
---
+++ Chapter_03/NumClassLibrary/NumClassLibrary/NumHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NumClassLibrary
+{
+    public class NumHelper
+    {
+        public long Factorial(int number)
+        {
+            if (number < 0)
+            {
+                throw new ArgumentException(
+                    string.Format("Factorial of negative number is undefined. You input value {0}", number));
+            }
+
+            if (number == 0)
+            {
+                return 1;
+            }
+
+            return number * Factorial(number - 1);
+        }
+    }
+}
