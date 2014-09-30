9b23e82ff65d4b996f2c9b4dcb9d8b1c618d7971
maltez <nick.lototskiy@gmail.com>
2014-09-30 17:16:31 +0300

    Add chapter 4 samples

---
+++ Chapter_04/ChapterFourExample/ChapterFourExample/Factorial.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ChapterFourExample
+{
+    public class Factorial : IDisposable
+    {
+        public int GetFactorial(int number)
+        {
+            if (number == 0)
+            {
+                return 1;
+            }
+
+            return number*GetFactorial(number - 1);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ Chapter_04/ChapterFourExample/ChapterFourExampleTest/FactorialTest.cs
@@ -0,0 +1,39 @@
+﻿using ChapterFourExample;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChapterFourExampleTest
+{
+    [TestClass]
+    public class FactorialTest
+    {
+        private Factorial _factorial;
+        private static TestContext _testContext;
+
+        [ClassInitialize]
+        public static void AssemblyInitalize(TestContext testContext)
+        {
+            _testContext = testContext;
+        }
+
+        [TestInitialize]
+        public void Initalize()
+        {
+            _factorial = new Factorial();
+        }
+
+        [TestMethod]
+        public void CheckFactorial_Positive_Test()
+        {
+            const int  expectedResult = 2;
+            int actualResult = _factorial.GetFactorial(2);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            _factorial.Dispose();
+        }
+    }
+}
